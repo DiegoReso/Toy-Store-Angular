f706bd56353bb8e6186a3263581aeda2504610b2
Diego Reis <diegoreso@me.com>
2024-04-25 10:25:32 -0300

    feat: add card grid list

--- src/app/components/toycard/toycard.component.ts
+++ src/app/components/toycard/toycard.component.ts
@@ -2,11 +2,12 @@ import { NgFor } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule } from '@angular/material/grid-list'
 
 @Component({
   selector: 'app-toycard',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule,NgFor],
+  imports: [MatCardModule, MatButtonModule,NgFor,MatGridListModule],
   templateUrl: './toycard.component.html',
   styleUrl: './toycard.component.css'
 })
