e95adc0e602c5469d4089358f646db9ab86bf038
Diego Reis <diegoreso@me.com>
2024-04-28 17:19:37 -0300

    feat: add create route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 
 import { ListCardsComponent } from './components/list-cards/list-cards.component';
+import { CreateComponent } from './components/create/create.component';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+  {
   path: '',
   component: ListCardsComponent
-}];
+  },
+  {
+    path: 'create-toy',
+    component: CreateComponent
+  }
+];
